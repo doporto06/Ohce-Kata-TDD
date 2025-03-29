70b59bfb3896226fe7f670d4f7fbf6ed9596cd0a
Diego Oporto <30302463+doporto06@users.noreply.github.com>
2025-03-28 22:28:47 -0300

    Added another test for afternoon edge case

--- test_ohce.py
+++ test_ohce.py
@@ -20,3 +20,7 @@ def test_greeting_morning_edge2():
 def test_greeting_afternoon():
     ohce = Ohce("Diego", datetime.datetime(2025, 3, 28, 15, 0, 0))
     assert ohce.greet() == "¡Buenas tardes Diego!"
+
+def test_greeting_afternoon_edge1():
+    ohce = Ohce("Diego", datetime.datetime(2025, 3, 28, 12, 0, 0))
+    assert ohce.greet() == "¡Buenas tardes Diego!"
