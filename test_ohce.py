27b55a243ea170a743fa58ca0d8a4b67249da499
Diego Oporto <30302463+doporto06@users.noreply.github.com>
2025-03-28 22:01:06 -0300

    Added another test for morning edge case
    This test checks one of the edge cases in the morning greeting

--- test_ohce.py
+++ test_ohce.py
@@ -2,6 +2,11 @@ import pytest
 import datetime
 from ohce import Ohce
 
+# Morning tests
 def test_greeting_morning():
     ohce = Ohce("Diego", datetime.datetime(2025, 3, 28, 9, 0, 0))
+    assert ohce.greet() == "¡Buenos días Diego!"
+
+def test_greeting_morning_edge1():
+    ohce = Ohce("Diego", datetime.datetime(2025, 3, 28, 6, 0, 0))
     assert ohce.greet() == "¡Buenos días Diego!"
