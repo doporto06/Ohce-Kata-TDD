be373a087a56ecf2e21918e0db339b1e2555abc9
Diego Oporto <30302463+doporto06@users.noreply.github.com>
2025-03-28 22:58:17 -0300

    Added test for reversing string

--- test_ohce.py
+++ test_ohce.py
@@ -2,6 +2,12 @@ import pytest
 import datetime
 from ohce import Ohce
 
+def test_reverse_string():
+    ohce = Ohce("Diego", datetime.datetime(2025, 3, 28, 9, 0, 0))
+    assert ohce.process_input("hola") == "aloh"
+
+
+### Test cases for the greetings based on the time of day ###
 # Morning tests
 def test_greeting_morning():
     ohce = Ohce("Diego", datetime.datetime(2025, 3, 28, 9, 0, 0))
