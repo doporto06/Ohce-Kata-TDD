7494008c2dcd717e3d3585d50f213a2973cfbc52
Diego Oporto <30302463+doporto06@users.noreply.github.com>
2025-03-28 22:12:16 -0300

    Added implementation in greet method to pass third test

--- ohce.py
+++ ohce.py
@@ -5,7 +5,7 @@ class Ohce:
 
     def greet(self):
         hour = self.time.hour
-        if hour == 6 or hour == 9:
+        if hour == 6 or hour == 9 or (hour == 11 and self.time.minute == 59):
             return "¡Buenos días Diego!"
 
     def process_input(self):
